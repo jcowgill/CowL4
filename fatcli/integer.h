cd2bd307fd379c53f3e7fc15cd9e44227a7f4dd4
James Cowgill <james410@cowgill.org.uk>
2013-04-13 00:53:07 +0100

    Added FAT CLI
    This is a (very) simple CLI for manipulating FAT images without having to
    mount them first.

---
+++ fatcli/integer.h
@@ -0,0 +1,27 @@
+/*-------------------------------------------*/
+/* Integer type definitions for FatFs module */
+/*-------------------------------------------*/
+
+#ifndef _INTEGER
+#define _INTEGER
+
+/* Get from stdint.h */
+#include <stdint.h>
+
+typedef int8_t   CHAR;
+typedef uint8_t  UCHAR;
+typedef uint8_t  BYTE;
+
+typedef int16_t  SHORT;
+typedef uint16_t USHORT;
+typedef uint16_t WORD;
+typedef uint16_t WCHAR;
+
+typedef int32_t  INT;
+typedef uint32_t UINT;
+
+typedef int32_t  LONG;
+typedef uint32_t ULONG;
+typedef uint32_t DWORD;
+
+#endif
