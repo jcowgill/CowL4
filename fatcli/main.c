434aad63bf31115e3a14d000539162cd3408c159
James Cowgill <james410@cowgill.org.uk>
2013-04-29 22:45:41 +0100

    Add copyright notice to fatcli/main.c

--- fatcli/main.c
+++ fatcli/main.c
@@ -1,3 +1,25 @@
+/*
+ * fatcli/main.c
+ * FAT CLI
+ *  Command line interface for FAT images
+ *  Uses FatFs http://elm-chan.org/fsw/ff/00index_e.html
+ *
+ * Copyright (C) 2013 James Cowgill
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,11 +27,6 @@
 #include <time.h>
 #include "ff.h"
 #include "diskio.h"
-
-// FAT CLI
-//  Command line interface for FAT images
-//  Uses FatFs http://elm-chan.org/fsw/ff/00index_e.html
-//
 
 // FAT Sector Size
 #define SECTOR_SIZE 512
